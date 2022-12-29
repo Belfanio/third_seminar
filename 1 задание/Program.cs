1c9d6b45942a7d3cead17eb78504709595d7dcb0
Ulitin Sergey <ulitin_sergey@mail.ru>
2022-12-29 20:26:14 +0300

    end homework

---
+++ 1 задание/Program.cs
@@ -0,0 +1,41 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            if (result > 9999 && result < 100000)
+            {
+                 break;
+            }
+            else
+            {
+                Console.WriteLine("Число не пятизначное");
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+
+int number = GetNumber("Введите число");
+int numberStart = number / 1000;
+int numberFinish = number % 100;
+if (numberStart / 10 == numberFinish % 10 && numberStart % 10 == numberFinish / 10)
+{
+    Console.WriteLine("Число является полиндромом");
+}
+else
+{
+   Console.WriteLine("Число не является полиндромом");
+}
---
+++ 2 задание/Program.cs
@@ -0,0 +1,36 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+        break;     
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+double getCoordinate(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double coordinate = Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2));
+    return coordinate;
+}
+
+int x1 = GetNumber("Введите координату x1 - ");
+int y1 = GetNumber("Введите координату y1 - ");
+int z1 = GetNumber("Введите координату z1 - ");
+int x2 = GetNumber("Введите координату x2 - ");
+int y2 = GetNumber("Введите координату y2 - ");
+int z2 = GetNumber("Введите координату z2 - ");
+
+double coordinate = getCoordinate(x1,y1,z1,x2,y2,z2);
+Console.WriteLine($"Расстояние между точками = {coordinate}");
